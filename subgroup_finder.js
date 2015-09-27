e86529c13aa697d8785d20726267e9d55c72c4b2
Hallie Lomax <rachel.lomax@bison.howard.edu>
2015-09-27 03:19:15 -0400

    a little cleaner

--- subgroup_finder.js
+++ subgroup_finder.js
@@ -1,32 +1,50 @@
 /*global console, $, callBackWithData, FB, hackers, groups*/
 
-function getGroupDataFromID(FB_ID) {
+var FB_ID,
+    subgroups;
+
+function getGroupLinks() {
     'use strict';
+    
     $(document).ready(function () {
-        var groups_to_join,
-            group_name,
-            g;
-        if (hackers.hasOwnProperty(FB_ID)) {
-            groups_to_join = hackers[FB_ID];
-            for (g in groups_to_join) {
-                if (groups_to_join.hasOwnProperty(g)) {
-                    group_name = groups_to_join[g];
-                    if (groups.hasOwnProperty(group_name)) {
-                        $("#recommendations").append(
-                            "<a href='" +  groups[group_name]
-                                + "'>" + group_name + "</a>"
+        $.getJSON('groups.json', function (data) {
+            var g, rec_group;
+            for (g in subgroups) {
+                if (subgroups.hasOwnProperty(g)) {
+                    rec_group = subgroups[g];
+                    if (data.hasOwnProperty(rec_group)) {
+                        $('#recommendations').append(
+                            "<a href='" + data[rec_group] + "'>"
+                                + rec_group
+                                + "</a>"
                         );
-                        console.log(group_name, groups[group_name]);
+                    } else {
+                        $('#recommendations').append(
+                            "<p>" + rec_group + "</p>"
+                        );
                     }
                 }
             }
-        }
+        });
     });
 }
 
-function grabFB_ID(e) {
+function getGroupDataFromID() {
+    'use strict';
+    $(document).ready(function () {
+        $.getJSON('hh_recommend.json', function (data) {
+            if (data.hasOwnProperty(FB_ID)) {
+                subgroups = data[FB_ID];
+                getGroupLinks();
+            }
+        });
+    });
+}
+
+function grabFB_ID() {
     "use strict";
 	FB.getLoginStatus(function (response) {
-        getGroupDataFromID(response.authResponse.userID);
+        FB_ID = response.authResponse.userID;
+        getGroupDataFromID();
     });
 }
