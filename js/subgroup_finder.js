21c67e6def2e541f7839fd044ee2e6ea94a54307
Hallie Lomax <rachel.lomax@bison.howard.edu>
2015-09-28 01:07:15 -0400

    Moving the js to a folder

---
+++ js/subgroup_finder.js
@@ -0,0 +1,70 @@
+/*global console, $, callBackWithData, FB, hackers, groups*/
+
+var FB_ID, FBName,
+    HH_ID, subgroups;
+
+function getGroupLinks() {
+    'use strict';
+    
+    $(document).ready(function () {
+        $.getJSON('js/groups.json', function (data) {
+            var g, rec_group;
+            for (g in subgroups) {
+                if (subgroups.hasOwnProperty(g)) {
+                    rec_group = subgroups[g];
+                    if (data.hasOwnProperty(rec_group)) {
+                        $('#recommendations').append(
+                            "<a href='" + data[rec_group] + "'>"
+                                + rec_group
+                                + "</a><br>"
+                        );
+                    } else {
+                        $('#recommendations').append(
+                            "<p>" + rec_group + "</p>"
+                        );
+                    }
+                }
+            }
+        });
+    });
+}
+
+function getGroupDataFromID() {
+    'use strict';
+    $(document).ready(function () {
+        $.getJSON('js/suggested_groups.json', function (data) {
+            if (data.hasOwnProperty(HH_ID)) {
+                subgroups = data[HH_ID];
+                getGroupLinks();
+            }
+        });
+    });
+}
+
+function getGroupID() {
+    'use strict';
+    $(document).ready(function () {
+        $.getJSON('js/names_ids.json', function (data) {
+            if (data.hasOwnProperty(FBName)) {
+                HH_ID = data[FBName][0];
+                getGroupDataFromID();
+            }
+        });
+    });
+}
+
+function grabFB_ID() {
+    "use strict";
+	FB.getLoginStatus(function (response) {
+        FB_ID = response.authResponse.userID;
+        FB.api(
+            '/' + FB_ID.toString(),
+            'GET',
+            {},
+            function (response2) {
+                FBName = response2.name;
+                getGroupID();
+            }
+        );
+    });
+}
